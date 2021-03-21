77d8fe3e8c0fda374e58ea0ba51cceb7bd7ab79d
gaetanoDJ <77935085+gaetanoDJ@users.noreply.github.com>
2021-03-21 17:22:50 -0500

    A4

--- Assignments/A4.R
+++ Assignments/A4.R
@@ -1,6 +1,7 @@
 library(FinTS)
 library(tseries)
 library(aTSA)
+library(urca)
 
 attach(irates)
 ts(r1)
@@ -12,19 +13,26 @@ tseries::adf.test(r3)
 Reg1 <-lm(r1 ~ r3)
 Reg2 <-lm(r3 ~ r1)
 
-summary(Reg1)
-summary(Reg2)
-coint.test(r1, r3)
+predReg1 <- Reg1$residuals
+tseries::adf.test(predReg1)
+
+
+adf <- ur.df(r1,type = c("trend"),selectlags = c("BIC"))
+summary(adf)
+# Unable to reject the null of a unit root as the calculated test statistic is -1.0409
+
+
+aTSA::coint.test(r1, r3)
 coint.test(r3,r1)
 
 # pvalue greater than 0.05 so we reject the null hypothesis that the two time series are not cointegrated
 
 
-bfx <- as.matrix(cbind(r1,r3), demean=FALSE)
+bfx <- as.matrix(cbind(r1,r3), demean=TRUE)
 po.test(bfx)
 
 bfx1 <- as.matrix(cbind(r3,r1), demean=FALSE)
 po.test(bfx1)
 
 
-# po.test also states that we fail to reject the null hypothesis that there is no cointegration.
+
